6900f36ec38ca906347e44b6fb59b82fcb8f056e
Mike Insch <mike@insch.info>
2020-12-26 18:42:40 +0000

    Initial Template

---
+++ src/devices/MicroDotPhat/MicroDotPhat.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.Gpio;
+using System.Device.I2c;
+using System.Device.Spi;
+
+namespace Iot.Device.MicroDotPhat
+{
+    /// <summary>
+    /// Add documentation here
+    /// </summary>
+    public class MicroDotPhat : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MicroDotPhat"/> class.
+        /// </summary>
+        public MicroDotPhat()
+        {
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+        }
+    }
+}
